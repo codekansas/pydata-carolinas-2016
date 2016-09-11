2bfde6f41ddb669a1e4eabf2e9fd4e8e02048927
Benjamin Bolte <bkbolte18@gmail.com>
2016-09-11 01:54:35 -0700

    first commit

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+DIRECTORY=$(pwd)
+
+# install pip
+{
+  which pip
+} || {
+  curl "https://bootstrap.pypa.io/get-pip.py" | python
+}
+
+# install virtualenv
+{
+  which virtualenv
+} || {
+  pip install virtualenv
+}
+
+if [ ! -d $DIRECTORY/venv ]; then
+  virtualenv $DIRECTORY/venv
+fi
+
+# activate virtual environment
+source $DIRECTORY/venv/bin/activate
+pip install --upgrade Theano keras h5py jupyter matplotlib
+
